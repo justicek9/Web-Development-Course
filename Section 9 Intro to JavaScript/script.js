fe92867687d2de6d208b6e2f32c848923b712d87
justicek9 <tony.justice@me.com>
2022-08-09 09:50:21 -0700

    Basic JavaScript Lesson

---
+++ Section 9 Intro to JavaScript/script.js
@@ -0,0 +1,6 @@
+alert("hello");
+alert("world")
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript//
+//alert is a function.  Hello is the message.  ; is the end//
+//https://github.com/rwaldron/idiomatic.js  use this for standarized formatting of code//
+
